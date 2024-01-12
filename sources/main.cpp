5de45c7d176d8e244c68fa23debd42d362563237
RoeschFlo <roesch.flo96@gmail.com>
2024-01-12 16:35:05 +0100

    trying to build now with with c-API, C++ connector crashes

--- sources/main.cpp
+++ sources/main.cpp
@@ -4,7 +4,8 @@
 #include "button.h"
 
 //Database
-#include "../database/include/mysql/jdbc.h"
+//#include "../database/include/mysql/jdbc.h"
+#include "jdbc.h"
 
 /*
 int main(void)
@@ -34,58 +35,20 @@ int main(void)
 using namespace std;
 
 //for demonstration only. never save your password in the code!
-const string server = "tcp://yourservername.mysql.database.azure.com:3306";
-const string username = "username@servername";
-const string password = "yourpassword";
+const sql::SQLString  server = "tcp://127.0.0.1:3306";
+const  sql::SQLString username = "root";
+const  sql::SQLString password = "yourpassword";
 
 int main()
 {
-	printf("War hier");
-	sql::Driver* driver;
-	sql::Connection* con;
-	sql::Statement* stmt;
-	sql::PreparedStatement* pstmt;
+    sql::mysql::MySQL_Driver* driver;
+    sql::Connection* con;
+    printf("1");
+    driver = sql::mysql::get_mysql_driver_instance();
+    printf("2");
+    con = driver->connect ("tcp://127.0.0.1:3306", "root", "yourpassword");
+    printf("3");
 
-	try
-	{
-		driver = get_driver_instance();
-		con = driver->connect(server, username, password);
-	}
-	catch (sql::SQLException e)
-	{
-		cout << "Could not connect to server. Error message: " << e.what() << endl;
-		system("pause");
-		exit(1);
-	}
-
-	//please create database "quickstartdb" ahead of time
-	con->setSchema("quickstartdb");
-
-	stmt = con->createStatement();
-	stmt->execute("DROP TABLE IF EXISTS inventory");
-	cout << "Finished dropping table (if existed)" << endl;
-	stmt->execute("CREATE TABLE inventory (id serial PRIMARY KEY, name VARCHAR(50), quantity INTEGER);");
-	cout << "Finished creating table" << endl;
-	delete stmt;
-
-	pstmt = con->prepareStatement("INSERT INTO inventory(name, quantity) VALUES(?,?)");
-	pstmt->setString(1, "banana");
-	pstmt->setInt(2, 150);
-	pstmt->execute();
-	cout << "One row inserted." << endl;
-
-	pstmt->setString(1, "orange");
-	pstmt->setInt(2, 154);
-	pstmt->execute();
-	cout << "One row inserted." << endl;
-
-	pstmt->setString(1, "apple");
-	pstmt->setInt(2, 100);
-	pstmt->execute();
-	cout << "One row inserted." << endl;
-
-	delete pstmt;
-	delete con;
-	system("pause");
-	return 0;
+    delete con;
+    return 0;
 }
