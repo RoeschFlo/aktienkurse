ca59cc96ea50fbbd42c098b1e16771baeee723d9
RoeschFlo <roesch.flo96@gmail.com>
2024-01-15 14:20:21 +0100

    now i have a compilable template with mysql c-api and raylib wrapped in a namespace

---
+++ external/raylib/include/cppwrapper.h
@@ -0,0 +1,22 @@
+// cppwrapper.h
+
+#ifndef CPPWRAPPER_H
+#define CPPWRAPPER_H
+
+namespace Cppwrapper {
+
+    
+        
+        #include "raylib.h"
+        #include "rlgl.h"
+    
+
+    // You can add more C functions within the namespace if needed
+    // For example:
+    // extern "C" {
+    //     #include "other_c_functions.h"
+    // }
+
+} // namespace MyWrapper
+
+#endif //
--- include/my_includes.h
+++ include/my_includes.h
@@ -3,4 +3,13 @@
 
 #include "../external/mysql_connector_c/include/mysql.h"
 
+
+//raylib includes
+
+#include "../external/raylib/include/cppwrapper.h"
+
+//glwf includes
+#include "../external/glfw/include/glfw3.h"
+
+
 #endif //MY_INCLUDES
